b9850293231b5f4dff1c8100e970ccd280a38e05
Lucas Barbosa Dourado <lucas.dourado@neomind.com.br>
2025-11-21 09:39:50 -0300

    fix: Removido os DTOs para FindAll, FindById, Update e Create, para utilização de um DTO de Response.

--- src/main/java/br/com/springnoobs/reminderapi/reminder/dto/response/CreateReminderResponseDTO.java
+++
@@ -1,5 +0,0 @@
-package br.com.springnoobs.reminderapi.reminder.dto.response;
-
-import java.time.Instant;
-
-public record CreateReminderResponseDTO(String title, Instant remindAt) {}
--- src/main/java/br/com/springnoobs/reminderapi/reminder/dto/response/UpdateReminderResponseDTO.java
+++
@@ -1,5 +0,0 @@
-package br.com.springnoobs.reminderapi.reminder.dto.response;
-
-import java.time.Instant;
-
-public record UpdateReminderResponseDTO(String title, Instant remindAt) {}
